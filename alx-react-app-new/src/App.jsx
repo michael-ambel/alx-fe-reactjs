3dffa3522465be4ded77be493a0dd1992a2166b0
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-08-14 19:50:37 +0300

    task 1

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,11 @@ function App() {
         <MainContent />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
         <Footer />
+        <Counter />
+
+
+
+
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
