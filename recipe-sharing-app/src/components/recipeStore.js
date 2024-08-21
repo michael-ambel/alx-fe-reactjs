c2f90af54a1f9ecafafab202a97e853f7ae6a053
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-08-21 17:45:32 +0300

    task 1

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,8 @@ import { create } from "zustand";
 const useRecipeStore = create((set) => ({
     recipes: [],
     addRecipe: (newRecipe) => set((state) => ({recipes: [...state.recipes, newRecipe]})),
+    deleteRecipe:(id) => set(),
+    updateRecipe:(id) => set(),
     setRecipes: (recipes) => set({recipes})
 }))
 
