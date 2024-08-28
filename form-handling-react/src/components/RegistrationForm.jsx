64b0c186aceaa904e0131eacaa1801af3d163aa1
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-08-28 22:55:23 +0300

    task 0

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 const RegistrationForm = () => {
     const [formData, setFormData] = useState({ username: '', password: '', email: '' });
+    const [errors, setErrors] = useState({ usernameError: '', passwordError: '', emailError: '' });
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -10,9 +11,18 @@ const RegistrationForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(formData.username === '' && formData.email === '' && formData.password === '' ){
-            console.log("Fild can not be blank");
-        }else{
+
+        if(!formData.username){
+            setErrors((prevState) => ({...prevState, usernameError:'Username can not be empty'}))
+        }
+        if(!formData.email){
+            setErrors((prevState) => ({...prevState, emailError:'Email can not be empty'}))
+        }
+        if(!formData.password){
+            setErrors((prevState) => ({...prevState, passwordError:'Password can not be empty'}))
+        }
+        
+        else{
            console.log(formData); 
         }
         
@@ -26,18 +36,21 @@ const RegistrationForm = () => {
                 value={username}
                 onChange={handleChange}
             />
+            <div>{errors.usernameError}</div>
             <input
                 type="email"
                 name="email"
                 value={email}
                 onChange={handleChange}
             />
+            <div>{errors.emailError}</div>
             <input
                 type="email"
                 name="password"
                 value={password}
                 onChange={handleChange}
             />
+            <div>{errors.passwordError}</div>
             <button type="submit">Submit</button>
         </form>
         
