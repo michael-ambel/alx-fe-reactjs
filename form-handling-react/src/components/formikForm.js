6d9777d0d6765180054300cf43a69d695ea90c4d
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-08-28 23:23:17 +0300

    task 0

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -2,13 +2,14 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const validationSchema = Yup.object({
-    name: Yup.string().required('Name is required'),
+    username: Yup.string().required('Name is required'),
     email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required')
 });
 
 const FormikForm = () => (
     <Formik
-        initialValues = {{ name: '', email: '' }}  // Corrected prop name
+        initialValues = {{ username: '', email: '', password: '' }}  // Corrected prop name
         validationSchema={validationSchema}
         onSubmit={(values) => {
             console.log(values);
@@ -16,10 +17,12 @@ const FormikForm = () => (
     >
         {() => (
             <Form>
-            <Field type="text" name="name" />
-            <ErrorMessage name="name" component="div" />
+            <Field type="text" name="username" />
+            <ErrorMessage name="username" component="div" />
             <Field type="email" name="email" />
             <ErrorMessage name="email" component="div" />
+            <Field type = "text" name = "password" />
+            <ErrorMessage name = "password" component = 'div' />
             <button type="submit">Submit</button>
         </Form>
         )}
