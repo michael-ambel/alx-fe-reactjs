86657e0a4fd2cdac5d497f670e3dee43e52e1e7e
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-08-28 23:17:48 +0300

    task 0

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,15 +2,17 @@
 
 import './App.css'
 import RegistrationForm from './components/RegistrationForm'
+import FormikForm from './components/formikForm'
 
 
 function App() {
 
   return (
-    <>
+    <div>
       <RegistrationForm />
+      <FormikForm />
 
-    </>
+    </div>
   )
 }
 
