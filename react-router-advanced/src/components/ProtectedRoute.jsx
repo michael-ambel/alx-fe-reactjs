3b8c64ff7492ec35e2f085a702dfaaa55c3b9bce
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-08-29 19:37:26 +0300

    task 2

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,28 +1,11 @@
 import { Navigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 
+const useAuth = () => {
+  return localStorage.getItem('authToken') !== null;
+};
 
 const ProtectedRoute = ({ element, ...rest }) => {
-    // Initialize auth state based on the presence of an auth token in localStorage
-    const [auth, setAuth] = useState(() => {
-      return localStorage.getItem('authToken') !== null;
-    });
-  
-    // Optionally, synchronize the auth state with localStorage in case the token changes.
-    useEffect(() => {
-      const handleStorageChange = () => {
-        setAuth(localStorage.getItem('authToken') !== null);
-      };
-  
-      // Listen for changes in localStorage
-      window.addEventListener('storage', handleStorageChange);
-  
-      return () => {
-        window.removeEventListener('storage', handleStorageChange);
-      };
-    }, []);
-  
-    return auth ? element : <Navigate to="/login" />;
-  };
-  
-  export default ProtectedRoute;
+  return useAuth() ? element : <Navigate to="/login" />;
+};
+
+export default ProtectedRoute;
