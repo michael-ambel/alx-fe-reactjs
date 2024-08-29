fe3871a08a9f93f2c24037eb2bf2d024f3af5093
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-08-29 12:39:36 +0300

    task 1

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -1,25 +1,27 @@
 import { useQuery } from 'react-query';
 
-const fetchData = async () => {
+const fetchPosts = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/posts');
     return res.json();
 };
 
 const PostsComponent = () => {
 
-    const { data, error, isLoading } = useQuery('fetchPosts', fetchData);
-    const isError = error;
-    
-    if (isLoading) return <div>Loading...</div>;
-    
-    if (isError) return <div>Error loading data</div>;
+    const { data, error, isLoading, isError, isFetching } = useQuery('fetchPosts', fetchPosts, { cacheTime: 1000 * 60 * 5, staleTime: 1000 * 60, refetchOnWindowFocus: true, keepPreviousData: true });
+
+      if (isLoading) return <div>Loading...</div>;
+      if (isError) return <div>Error: {error.message}</div>;
 
 
     return (
         <div>
-            {data.map(item => (
-                <div key={item.id}>{item.name}</div>
-            ))}
+            <h1>Posts</h1>
+            {isFetching && <div>Updating...</div>}
+            <ul>
+                {data.map(post => (
+                <li key={post.id}>{post.title}</li>
+                ))}
+            </ul>
         </div>
     );
 };
