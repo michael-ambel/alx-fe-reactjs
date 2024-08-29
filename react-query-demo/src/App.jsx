21118a8e1a746510096918b245f035ff14d261cf
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-08-29 12:19:35 +0300

    task 1

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client = {queryClient}>
+    <QueryClientProvider client={queryClient}>
       <PostsComponent />
     </QueryClientProvider>
   )
