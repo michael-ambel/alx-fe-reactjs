1b4cfe2b7e248fcbe172bc91927159e7f9de9891
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-08-31 21:32:24 +0300

    task 3

---
+++ react-todo/src/__tests__/TodoList.test.js
@@ -0,0 +1,49 @@
+const { render, screen, fireEvent } = require('@testing-library/react');
+require('@testing-library/jest-dom');
+const { jest, test, expect } = require('@jest/globals');
+const TodoList = require('./TodoList');
+
+
+test('render TodoList component', () => {
+    render(<TodoList />);
+    expect(screen.getByText('Learn React')).toBeInTheDocument()
+    expect(screen.getByText('Build a Todo List')).toBeInTheDocument()
+    expect(screen.getByText('Master JavaScript')).toBeInTheDocument()
+})
+
+test('add new TodoList functionality', () => {
+    render(<TodoList />);
+    
+    const input = screen.getByPlaceholderText('Add a todo');
+    const button = screen.getByText('Add Todo');
+
+    fireEvent.change(input, { target: { value: 'TypeScript' } });
+
+    fireEvent.click(button);
+  
+    expect(screen.getByText('TypeScript')).toBeInTheDocument();
+})
+
+test('toggles a todo item from incomplete to complete', () => {
+    render(<TodoList />);
+    expect(screen.getByLabelText('Learn React')).not.toBeChecked();
+    fireEvent.click(screen.getByLabelText('Learn React'));
+    expect(screen.getByLabelText('Learn React')).toBeChecked();
+  });
+  
+  test('toggles a todo item from complete to incomplete', () => {
+    render(<TodoList />);
+    fireEvent.click(screen.getByLabelText('Learn React'));
+    fireEvent.click(screen.getByLabelText('Learn React'));
+    expect(screen.getByLabelText('Learn React')).not.toBeChecked();
+  });
+
+test('delete Todo item', () => {
+    render(<TodoList />);
+    const deleteButton = screen.getAllByText('Delete')
+
+    fireEvent.click(deleteButton[0])
+
+    expect(screen.queryByText('Learn React')).not.toBeInTheDocument();
+
+})
