be2f7f8cb80c9f765e8f642b0bb11de71ac85cac
michael-ambel <163504211+michael-ambel@users.noreply.github.com>
2024-09-03 22:18:35 +0300

    task 1

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false,
+  // darkMode: false,
   content: [],
   theme: {
     extend: {},
