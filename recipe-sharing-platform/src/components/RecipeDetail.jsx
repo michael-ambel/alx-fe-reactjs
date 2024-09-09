74dbc17251f2dd23e3187359ba54656e672fef53
michael-ambel <michaelambel46@gmail.com>
2024-09-09 22:15:20 +0300

    task 2

--- recipe-sharing-platform/src/components/RecipeDetail.jsx
+++ recipe-sharing-platform/src/components/RecipeDetail.jsx
@@ -14,12 +14,14 @@ const RecipeDetail = () => {
     }, [])
     return ( 
         <div>
-            <div className="flex flex-col items-center justify-center w-[400px] h-[400px] bg-slate-300 p-4 rounded-2xl shadow-sm shadow-slate-500 hover:scale-105 transition-transform transform">
-                <img className="w-40 h-40 object-cover" src={data.image} alt={data.title} />
-                <h2 className="text-3xl font-bold ">{data.title}</h2>
-                <p>{data.summary}</p>
-                <p>{data.ingredients}</p>
-                <p>{data.instructions}</p>
+            <div className="flex flex-col md:flex-row items-center justify-center w-auto h-auto gap-8 bg-slate-300 p-4 rounded-2xl shadow-sm shadow-slate-500 hover:scale-105 transition-transform transform">
+                <img className="w-40 md:w-60 h-40 md:h-60 object-cover" src={data.image} alt={data.title} />
+                <div className='flex flex-col gap-4 items-start justify-start'>
+                    <h2 className="text-3xl font-bold ">{data.title}</h2>
+                    <p>{data.summary}</p>
+                    <p>{data.ingredients}</p>
+                    <p>{data.instructions}</p>
+                </div>
             </div>
         </div>
      );
