b0ff848677753bace9f7949ac2d8f54fe346ffd6
michael-ambel <michaelambel46@gmail.com>
2024-09-09 20:41:32 +0300

    task 1

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
 
   return (
     <div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {data.map((rec, index) => (
         <div className="flex flex-col items-center justify-center w-[400px] h-[400px] bg-slate-300 p-4 rounded-2xl shadow-sm shadow-slate-500 hover:scale-105 transition-transform transform" key={index}>
                 <img className="w-40 h-40 object-cover" src={rec.image} alt={rec.title} />
